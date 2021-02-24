e6e98085bc5d92d32f9144626137e05f92f9cd51
tonghs <tonghuashuai@gmail.com>
2021-02-24 09:57:14 +0800

    fix

--- pages/account/index.js
+++ pages/account/index.js
@@ -51,6 +51,7 @@ Page({
         }
       })
     }
+    this.login()
   },
   getUserInfo(e) {
     app.globalData.userInfo = e.detail.userInfo
@@ -64,93 +65,98 @@ Page({
       title: '登录中',
     })
     var _self = this
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-    wx.login({
-      success (res) {
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: req.getUrl('/account/wxapp/login/'),
-            method: 'POST',
-            data: {
-              code: res.code,
-              user_name: app.globalData.userInfo.nickName
-            },
-            success (res) {
-              if (res.statusCode != 200) {
-                var msg = ""
-                if (res.data.msg !== undefined ) {
-                  msg = res.data.msg
-                } else {
-                  msg = '错误代码：' + res.statusCode
-                }
-                wx.hideLoading({
-                  success: (res) => {},
-                })
-                wx.showModal({
-                  title: '登录失败',
-                  content: msg
-                })
-              } else {
-                var ret = res.data.content
-                wx.showToast({
-                  title: '登录成功',
-                })
-                var access_token = ret.access_token
-                var refresh_token = ret.refresh_token
-                var user_id = ret.user_id
-                var user_name = ret.user_name
-                var is_admin = ret.is_admin
-                _self.setData({
-                  is_logined: true,
-                  appUserInfo: {
-                    user_id: user_id,
-                    user_name: user_name,
-                    is_admin: is_admin
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        wx.login({
+          success (res) {
+            if (res.code) {
+              //发起网络请求
+              wx.request({
+                url: req.getUrl('/account/wxapp/login/'),
+                method: 'POST',
+                data: {
+                  code: res.code,
+                  user_name: app.globalData.userInfo.nickName
+                },
+                success (res) {
+                  if (res.statusCode != 200) {
+                    var msg = ""
+                    if (res.data.msg !== undefined ) {
+                      msg = res.data.msg
+                    } else {
+                      msg = '错误代码：' + res.statusCode
+                    }
+                    wx.hideLoading({
+                      success: (res) => {},
+                    })
+                    wx.showModal({
+                      title: '登录失败',
+                      content: msg
+                    })
+                  } else {
+                    var ret = res.data.content
+                    wx.showToast({
+                      title: '登录成功',
+                    })
+                    var access_token = ret.access_token
+                    var refresh_token = ret.refresh_token
+                    var user_id = ret.user_id
+                    var user_name = ret.user_name
+                    var is_admin = ret.is_admin
+                    _self.setData({
+                      is_logined: true,
+                      appUserInfo: {
+                        user_id: user_id,
+                        user_name: user_name,
+                        is_admin: is_admin
+                      }
+                    })
+                    app.globalData.appUserInfo = {
+                      user_id: user_id,
+                      user_name: user_name,
+                      is_admin: is_admin
+                    }
+                    app.globalData.is_logined = true
+    
+                    wx.setStorage({
+                      data: access_token,
+                      key: 'access_token',
+                    })
+                    wx.setStorage({
+                      data: refresh_token,
+                      key: 'refresh_token',
+                    })
+    
+                    wx.hideLoading({
+                      success: (res) => {},
+                    })
                   }
-                })
-                app.globalData.appUserInfo = {
-                  user_id: user_id,
-                  user_name: user_name,
-                  is_admin: is_admin
+                },
+                fail (res) {
+                  wx.showToast({
+                    title: '登录失败',
+                  })
+                  wx.hideLoading({
+                    success: (res) => {},
+                  })
                 }
-                app.globalData.is_logined = true
-
-                wx.setStorage({
-                  data: access_token,
-                  key: 'access_token',
-                })
-                wx.setStorage({
-                  data: refresh_token,
-                  key: 'refresh_token',
-                })
-
-                wx.hideLoading({
-                  success: (res) => {},
-                })
-              }
-            },
-            fail (res) {
+              })
+            } else {
               wx.showToast({
-                title: '登录失败',
+                title: '登录失败！' + res.errMsg,
               })
               wx.hideLoading({
                 success: (res) => {},
               })
             }
-          })
-        } else {
-          wx.showToast({
-            title: '登录失败！' + res.errMsg,
-          })
-          wx.hideLoading({
-            success: (res) => {},
-          })
-        }
+          }
+        })
+        
       }
     })
     
