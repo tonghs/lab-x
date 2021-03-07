be680b503a18785495477cbd9dc542576ce2b7f3
tonghs <tonghuashuai@gmail.com>
2021-03-08 00:37:56 +0800

    文档上传

--- pages/document/index.js
+++ pages/document/index.js
@@ -17,10 +17,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      selectFile: this.selectFile.bind(this),
-      uplaodFile: this.uplaodFile.bind(this)
-  })
   },
 
   /**
@@ -71,63 +67,10 @@ Page({
   onShareAppMessage: function () {
 
   },
-  chooseImage: function (e) {
-    var that = this;
-    wx.chooseImage({
-        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-        success: function (res) {
-            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-            that.setData({
-                files: that.data.files.concat(res.tempFilePaths)
-            });
-        }
-    })
-},
-  previewImage: function(e){
-      wx.previewImage({
-          current: e.currentTarget.id, // 当前显示图片的http链接
-          urls: this.data.uploadedFileUrls // 需要预览的图片http链接列表
-      })
-  },
-  selectFile(files) {
-      // console.log('files', files)
-      // 返回false可以阻止某次文件上传
-  },
-  uplaodFile(files) {
-      var _self = this
-      // console.log('upload files', files)
-      // 文件上传的函数，返回一个promise
-      return new Promise((resolve, reject) => {
-        for (var i = 0; i < files.tempFilePaths.length; i++) {
-          cos.uploadFile({
-            path: files.tempFilePaths[i],
-            success: (res) => {
-              _self.data.uploadedKeys.push(res.key)  // 存储 key
-              _self.data.uploadedFileUrls.push({url: res.url + "?imageView2/q/85"})
-              if (_self.data.uploadedFileUrls.length >= files.tempFilePaths.length) {
-                resolve({
-                  urls: files.tempFilePaths
-                })
-              }
-            },
-            fail: () => {
-              reject(res)
-            }
-          })
-        }
-      })
-  },
-  uploadError(e) {
-      // console.log('upload error', e.detail)
-      wx.showModal({title: '图片尺寸过大！', showCancel: false, });
-  },
-  uploadSuccess(e) {
-      // console.log('upload success', e.detail)
-      // wx.showModal({title: '上传成功', showCancel: false});
-    // console.log(this.data.uploadedKeys)
-    this.setData({
-      files: this.data.uploadedFileUrls  // 替换 tmp 路径为线上 url
+
+  navToUploader: function() {
+    wx.navigateTo({
+      url: '/pages/document/upload/index',
     })
   }
 })
