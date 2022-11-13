bec4c8b8dc3dcc31e2f8cf03be42193ee1e00fa6
tonghs <tonghuashuai@gmail.com>
2022-11-13 23:12:01 +0800

    图片显示质量

--- pages/document/upload/index.js
+++ pages/document/upload/index.js
@@ -75,19 +75,6 @@ Page({
   onShareAppMessage: function () {
 
   },
-  chooseImage: function (e) {
-    var that = this;
-    wx.chooseImage({
-        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-        success: function (res) {
-            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-            that.setData({
-                files: that.data.files.concat(res.tempFilePaths)
-            });
-        }
-    })
-},
   previewImage: function(e){
       wx.previewImage({
           current: e.currentTarget.id, // 当前显示图片的http链接
@@ -110,7 +97,7 @@ Page({
             path: files.tempFilePaths[i],
             success: (res) => {
               _self.data.uploadedKeys.push(res.key)  // 存储 key
-              _self.data.uploadedFileUrls.push({url: res.url + "?imageView2/q/85"})
+              _self.data.uploadedFileUrls.push({url: res.url + "?imageView2/2/w/1024/q/85"})
               if (_self.data.uploadedFileUrls.length >= files.tempFilePaths.length) {
                 resolve({
                   urls: files.tempFilePaths
